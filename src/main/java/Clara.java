7136df9e0e2eb701cf6c763f2c45c3c0ceb0595d
amberrrruby <amberrrruby.92@proton.me>
2026-08-13 23:02:45 +0800

    feat(level-0): rename chatbot; update greeting

---
+++ src/main/java/Clara.java
@@ -0,0 +1,11 @@
+public class Clara {
+    public static void main(String[] args) {
+		String greetingLine = "\nHello. I'm Clara.\nHow can I assist you?";
+        System.out.println(greetingLine);
+
+		System.out.print('\n');
+
+		String exitLine = "Goodbye. See you again.";
+		System.out.println(exitLine);
+    }
+}
