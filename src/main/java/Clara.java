66b6fae83e3ca28d28418572c3d71fea4d054890
amberrrruby <amberrrruby.92@proton.me>
2026-08-14 10:48:41 +0800

    feat(level-1): implement echo

--- src/main/java/Clara.java
+++ src/main/java/Clara.java
@@ -1,11 +1,25 @@
+import java.util.Scanner;
+
 public class Clara {
-    public static void main(String[] args) {
-		String greetingLine = "\nHello. I'm Clara.\nHow can I assist you?";
-        System.out.println(greetingLine);
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    String greetingLine = "\n[Clara] Hello. I'm Clara. How can I assist you?";
+    System.out.println(greetingLine);
 
-		System.out.print('\n');
+    while (true) {
+      System.out.print(">> ");
+      String userInput = scanner.nextLine();
 
-		String exitLine = "Goodbye. See you again.";
-		System.out.println(exitLine);
+      // TODO: Simplified parsing model. Only echo supported at Level 1.
+      if ("bye".equals(userInput)) {
+        break;
+      }
+
+      String echoString = "\n[Clara] (echoed:) " + userInput;
+      System.out.println(echoString);
     }
+
+    String exitLine = "[Clara] Goodbye. See you again.";
+    System.out.println(exitLine);
+  }
 }
